88b2208c12d5c973bd2ffa50924f881db8883878
Dorongyee <99232293+Dorongyee@users.noreply.github.com>
2024-04-30 10:26:40 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -7,7 +7,10 @@ def solution(numbers):
     str_numbers = list(map(str, numbers))
     # custom_key를 이용하여 정렬
     sorted_numbers = sorted(str_numbers, key=custom_key, reverse=True)
+    
+    #0 인 경우를 추가해줘야 함! (edge case존재 -> 이걸 str(int()) 로 푼 사람도 있음.)
     if sorted_numbers[0] == "0":
         return "0"
+    
     result = ''.join(sorted_numbers)
     return result
