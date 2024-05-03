ea4e2e8883107dac49dd9bdc79c74103f48d5c09
Dorongyee <99232293+Dorongyee@users.noreply.github.com>
2024-05-03 16:52:07 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,8 @@
 def solution(citations):
     citations.sort(reverse=True)
     n = len(citations)
+    # 인용횟수가 전부 전체 갯수 이상인경우 h index는 논문 수; return n
+    # 그렇지 않은 경우부터
     for i in range(n):
         if citations[i] < i + 1:
             return i
